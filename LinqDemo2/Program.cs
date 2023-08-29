f0441f78f6feab491a6cd281560c4b8e45cef780
unknown <Bhagya>
2023-08-29 22:18:19 +0530

    commit for linq

---
+++ LinqDemo2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LinqDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = { "10", "5", "100", "30", "50", "abc", "pqr" };
+            var numericStrings = numbers.Where(x => int.TryParse(x, out int n));
+            var numericValues = numericStrings.Select(x => int.Parse(x));
+
+            if (numericValues.Any())
+            {
+                int maxNum=numericValues.Max();
+                int minNum=numericValues.Min();
+                int secondMax=numericValues.OrderByDescending(n=>n).Skip(1).FirstOrDefault();
+                int sumOf=numericValues.Sum();
+                Console.WriteLine($"Maximum number is {maxNum}");
+                Console.WriteLine($"Minmum number is {minNum}");
+                Console.WriteLine($"Second maximum number is {secondMax}");
+                Console.WriteLine($"Sum of numbers are {sumOf}");
+
+                var descendingNum = numericValues.OrderByDescending(n => n);
+                foreach( var n in descendingNum )
+                {
+                    Console.WriteLine(n);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No valid numbers are foun in list");
+            }
+        }
+    }
+}
